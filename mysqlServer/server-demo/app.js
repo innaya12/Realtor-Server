92fc5f8dfd985b696d61b9140b0d44251da191cc
unknown <yakubov.inna2@gmail.com>
2020-01-17 15:40:45 +0200

    added add apartment form

--- mysqlServer/server-demo/app.js
+++ mysqlServer/server-demo/app.js
@@ -5,13 +5,12 @@ var logger = require('morgan');
 var cors = require('cors');
 
 var indexRouter = require('./routes/indexRoute');
+var addapartmentRouter = require('./routes/addApartmentRouter');
 var apartmentsRouter = require('./routes/apartmentsRoute');
+var imagesRouter = require('./routes/imagesRoute');
 var loginRouter = require('./routes/loginRouter');
 var signupRouter = require('./routes/signupRouter');
-var imagesRouter = require('./routes/imagesRoute');
 var app = express();
-// var usersRouter = require('./routes/usersExample');
-// var ordersRouter = require('./routes/ordersExample').router;
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,16 +21,25 @@ app.use(cors());
 // app.use(cors({credentials: true, origin:'http://localhost:3000'}));
 
 app.use('/', indexRouter);
-app.use('/login', loginRouter);
-app.use('/signup', signupRouter);
-app.use('/images', imagesRouter);
-
+app.use('/addapartment', addapartmentRouter);
 app.use('/apartments',(req, res, next) => {
     res.cookie('my-cookie', JSON.stringify({userId: 1234, role:'user'}), {maxAge: 1000 *60 *60 *24});
     next();
 }, apartmentsRouter);
+app.use('/images', imagesRouter);
+app.use('/login', loginRouter);
+app.use('/signup', signupRouter);
+
+module.exports = app;
+
+
+
+
+
+
+
+// var usersRouter = require('./routes/usersExample');
+// var ordersRouter = require('./routes/ordersExample').router;
 
 // app.use('/users', usersRouter);
 // app.use('/orders', ordersRouter);
-
-module.exports = app;
--- mysqlServer/server-demo/routes/signupRouter.js
+++ mysqlServer/server-demo/routes/signupRouter.js
@@ -10,7 +10,7 @@ router.post('/', function(req, res, next) {
         res.send('all fields are required');
         return;
     }else{
-        signup.sendUser(firstName, lastName,email, password, phone)
+        signup.addUser(firstName, lastName,email, password, phone)
         .then(success => res.status(200).json({status: 200, success: success}))
         .catch(error => res.status(401).json({status: 401, error: 'one of more of the inputs are invalid! '}))
     }
